9a90bb07f19eb955960c749ec9a7711620bb1fd8
nartkosova <nartkosova21@gmail.com>
2025-04-11 15:47:03 +0200

    new build

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
-import express from 'express'
-import path from 'path'
+import express from "express";
+import path from "path";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-// Serve static files from the 'build' directory
-app.use(express.static(path.join(__dirname, '../build')))
+app.use(express.static(path.join(__dirname, "../build")));
 
-// Serve the main HTML file for all unmatched routes (SPA)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build/index.html'))
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`)
-})
+  console.log(`Server is running on http://localhost:${port}`);
+});
